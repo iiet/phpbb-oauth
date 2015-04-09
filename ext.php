9efb2c22cf13e3e299c9eb3e88c29e5057acca5b
Mateusz Małek <mamalek@student.agh.edu.pl>
2015-04-09 11:06:23 +0200

    Disable/enable account-related UCP modules on OAuth enable/disable

--- ext.php
+++ ext.php
@@ -4,4 +4,45 @@ namespace iiet\oauth;
 
 class ext extends \phpbb\extension\base
 {
+	function enable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '':
+				$db = $this->container->get('dbal.conn');
+				$modules = array('UCP_PROFILE_REG_DETAILS', 'UCP_PROFILE_AUTOLOGIN_KEYS', 'UCP_AUTH_LINK_MANAGE');
+				$sql = 'UPDATE ' . MODULES_TABLE . '
+					SET module_enabled = 0
+					WHERE ' . $db->sql_in_set('module_langname', $modules);
+				$db->sql_query($sql);
+
+				return 'ucp_modules_disabled';
+			break;
+
+			default:
+				return parent::enable_step($old_state);
+			break;
+		}
+	}
+
+	function disable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '':
+				$db = $this->container->get('dbal.conn');
+				$modules = array('UCP_PROFILE_REG_DETAILS', 'UCP_PROFILE_AUTOLOGIN_KEYS', 'UCP_AUTH_LINK_MANAGE');
+				$sql = 'UPDATE ' . MODULES_TABLE . '
+					SET module_enabled = 1
+					WHERE ' . $db->sql_in_set('module_langname', $modules);
+				$db->sql_query($sql);
+
+				return 'ucp_modules_disabled';
+			break;
+
+			default:
+				return parent::disable_step($old_state);
+			break;
+		}
+	}
 }
